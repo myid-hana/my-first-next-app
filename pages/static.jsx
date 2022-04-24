19a535fb7c901035dfda9ec0e2c12927f73bd12f
hana.park <hana.park@quantit.io>
2022-04-24 11:57:02 +0900

    3.2.1 타입스크립트 프로젝트 시작하기

--- pages/static.jsx
+++
@@ -1,9 +0,0 @@
-const staticPage = ({ time }) => {
-    return <div>{time}</div>
-};
-
-export const getStaticProps = async () => {
-    return { props: { time: new Date().toISOString() } };
-};
-
-export default staticPage;
